a6fb5ecb9148f0f98595ddd920740b857739ad5c
MurreLau <353765792@qq.com>
2020-12-28 09:11:33 +0800

    二分查找解题思路 解决 基本的二分查找,使用区间思想解决+1-1的困扰 增加注释,加强理解

--- src/main/java/labuladong/binarysearch/BasicBinarySearch.java
+++ src/main/java/labuladong/binarysearch/BasicBinarySearch.java
@@ -1,5 +1,9 @@
 package labuladong.binarysearch;
 
+/**
+ * 最基础的二分查找
+ * 注意边界问题,搜索区间全部以闭区间的概念来理解[0,N]
+ */
 public class BasicBinarySearch {
     public static void main(String[] args) {
         BasicBinarySearch binarySearch = new BasicBinarySearch();
@@ -11,16 +15,20 @@ public class BasicBinarySearch {
     int binarySearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        //因为是闭区间,所以必须left>right才不符合条件,否则会错过一部分
         while (left <= right) {
+            //这里是一个小优化,降低溢出的概率
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if (nums[mid] == target) {//当中间就是目标时直接返回
                 return mid;
-            } else if (target < nums[mid]) {
+            } else if (target < nums[mid]) {//当目标小于中间值时,即mid不是,那么我们直接从闭合区间[LEFT,mid-1]找
+                //为什么是-1,因为我们的搜索区间是闭区间
                 right = mid - 1;
-            } else if (target > nums[mid]) {
+            } else if (target > nums[mid]) {//当目标大于中间值时,即mid不是,那么我们直接从闭合区间[mid+1,right]找
                 left = mid + 1;
             }
         }
+        //找不到值,直接返回-1
         return -1;
     }
 }
