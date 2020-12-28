25ab8d29349125b598a52c5d5f4feaabd20d9273
MurreLau <353765792@qq.com>
2020-12-28 17:14:09 +0800

    滑动窗口 还有问题

---
+++ src/main/java/labuladong/scroll_window/MinWindow.java
@@ -0,0 +1,63 @@
+package labuladong.scroll_window;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MinWindow {
+    public static void main(String[] args) {
+        System.out.println(minWindow("ADOBECODEBANC", "ABC"));
+    }
+
+    static String minWindow(String s, String t) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+//        unordered_map<char, int> need, window;
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+        // 记录最小覆盖子串的起始索引及长度
+        int start = 0, len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            // c 是将移入窗口的字符
+            char c = s.charAt(right);
+            // 右移窗口
+            right++;
+            // 进行窗口内数据的一系列更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+//                window[c]++;
+//                if (window[c] == need[c])
+                if (window.get(c) == need.get(c)) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧窗口是否要收缩
+            while (valid == need.size()) {
+                // 在这里更新最小覆盖子串
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                // d 是将移出窗口的字符
+//                char d = s[left];
+                char d = s.charAt(left);
+                // 左移窗口
+                left++;
+                // 进行窗口内数据的一系列更新
+                if (need.containsKey(d)) {
+                    if (window.get(d) == need.get(d))
+                        valid--;
+//                    window[d]--;
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+        // 返回最小覆盖子串
+        return len == Integer.MAX_VALUE ?
+                "" : s.substring(start, len);
+    }
+}
