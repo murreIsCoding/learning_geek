3fa46c601847fd597537894acf1abc3b4a9aedf8
MurreLau <353765792@qq.com>
2021-01-14 10:18:56 +0800

    子集切割问题:补充状态压缩法

--- src/main/java/labuladong/dynamic_program/package_problem/CanPartition.java
+++ src/main/java/labuladong/dynamic_program/package_problem/CanPartition.java
@@ -1,20 +1,20 @@
 package labuladong.dynamic_program.package_problem;
 
-/**
- * leetcode 416题
- * 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
- * 注意:
- * 每个数组中的元素不会超过 100
- * 数组的大小不会超过 200
- * 示例 1:
- * 输入: [1, 5, 11, 5]
- * 输出: true
- * 解释: 数组可以分割成 [1, 5, 5] 和 [11].
- *  
- * 示例 2:
- * 输入: [1, 2, 3, 5]
- * 输出: false
- * 解释: 数组不能分割成两个元素和相等的子集.
+/*
+  leetcode 416题
+  给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+  注意:
+  每个数组中的元素不会超过 100
+  数组的大小不会超过 200
+  示例 1:
+  输入: [1, 5, 11, 5]
+  输出: true
+  解释: 数组可以分割成 [1, 5, 5] 和 [11].
+   
+  示例 2:
+  输入: [1, 2, 3, 5]
+  输出: false
+  解释: 数组不能分割成两个元素和相等的子集.
  */
 
 /**
@@ -58,6 +58,39 @@ public class CanPartition {
             }
         }
         return dp[nums.length][half];
+    }
+
+    /**
+     * 装填压缩版本,由于dp[i][j]的状态都是由dp[i-1][j]的状态得来的,所以二维数组可以压缩成一维数组,只需要使用dp[j]来表示dp[i-1][j]即可
+     *
+     * @param nums
+     * @return
+     */
+    boolean canPartitionStateCompression(int[] nums) {
+        int sum = sum(nums);
+        //如果无法被整除直接返回false
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int half = sum / 2;
+        //创建DP数组
+        boolean[] dp = new boolean[half + 1];
+        //初始化base case
+        dp[0] = true;
+        for (int i = 1; i <= nums.length; i++) {
+            //注意,如果是状态压缩情况下,必须从后面开始往前遍历,否则将出错,因为上一次的结果已经被这一次的所覆盖,而反向遍历不会出现这个情况
+            for (int j = half; j > 0; j--) {
+                //判断是否能装下,如果根本装不下就只能不装了--即沿用上一次的结果所以无需处理
+                if (j - nums[i - 1] >= 0) {
+                    //使用逻辑或,只要有一种情况能装下就是能装下
+                    dp[j] = Boolean.logicalOr(
+                            dp[j],//不装
+                            dp[j - nums[i - 1]]//装,注意 i-1表示当前的数
+                    );
+                }
+            }
+        }
+        return dp[half];
     }
 
     private int sum(int[] nums) {
--- src/test/java/labuladong/dynamic_program/package_problem/CanPartitionTest.java
+++ src/test/java/labuladong/dynamic_program/package_problem/CanPartitionTest.java
@@ -22,13 +22,34 @@ public class CanPartitionTest {
     public void canPartition() {
         int[] nums = {1, 5, 11, 5};
         boolean res = canPartition.canPartition(nums);
-        Assert.assertEquals(res, true);
+        Assert.assertEquals(true, res);
     }
 
     @Test
     public void canPartition2() {
         int[] nums = {1, 2, 3, 5};
         boolean res = canPartition.canPartition(nums);
-        Assert.assertEquals(res, false);
+        Assert.assertEquals(false, res);
+    }
+
+    @Test
+    public void canPartition_ds_1() {
+        int[] nums = {1, 5, 11, 5};
+        boolean res = canPartition.canPartitionStateCompression(nums);
+        Assert.assertEquals(true, res);
+    }
+
+    @Test
+    public void canPartition_ds_2() {
+        int[] nums = {1, 2, 3, 5};
+        boolean res = canPartition.canPartitionStateCompression(nums);
+        Assert.assertEquals(false, res);
+    }
+
+    @Test
+    public void canPartition_ds_leetcode() {
+        int[] nums = {1, 2, 5};
+        boolean res = canPartition.canPartitionStateCompression(nums);
+        Assert.assertEquals(false, res);
     }
 }
